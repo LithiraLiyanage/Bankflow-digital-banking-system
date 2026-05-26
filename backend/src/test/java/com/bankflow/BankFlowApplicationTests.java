440658bd0511d7b59e31dacd74dfebf1d18c2175
Lithira Liyanage <lithiraliyanage666@gmail.com>
2026-05-26 20:28:16 +0530

    test: add Spring Boot context test

---
+++ backend/src/test/java/com/bankflow/BankFlowApplicationTests.java
@@ -0,0 +1,10 @@
+package com.bankflow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankFlowApplicationTests {
+    @Test
+    void contextLoads() {}
+}
